d9f6d219af93bfb01b08adc7560be5ef1988c525
John Newton <ox37ex@hotmail.com>
2022-12-02 12:11:44 +0000

    Another slight tweak on Branch1

--- Blink/blink/main/blink_example_main.c
+++ Blink/blink/main/blink_example_main.c
@@ -86,6 +86,7 @@ newtest.first = 0;
     while (1) {
         ESP_LOGI(TAG, "Turning the LED on or off: %s!", s_led_state == true ? "ON" : "OFF");
         blink_led();
+        blink_led();
         /* Toggle the LED state */
         s_led_state = !s_led_state;
         vTaskDelay(CONFIG_BLINK_PERIOD / portTICK_PERIOD_MS);
